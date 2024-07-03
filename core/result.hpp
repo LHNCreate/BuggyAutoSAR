8a1478487c84166dfa541700de86395461d5905a
lihaonan <ext-LiHaonan@csvw.com>
2024-07-03 10:49:39 +0800

    result 大体设计完成

--- core/result.hpp
+++ core/result.hpp
@@ -302,19 +302,19 @@ public:
 
     // Implementation - [SWS_CORE_00763]
     template<typename G>
-    E ErrorOr(G&& defaultValue) const&
+    E ErrorOr(G&& defaultError) const&
     {
-        if (!HasValue()) {
+        if (std::holds_alternative<E>(value_)) {
             return std::get<E>(value_);
         }
-        return static_cast<E>(std::forward<G>(defaultValue));
+        return static_cast<E>(std::forward<G>(defaultError));
     }
 
     // Implementation - [SWS_CORE_00764]
     template<typename G>
     E ErrorOr(G&& defaultError) &&
     {
-        if (!HasValue()) {
+        if (std::holds_alternative<E>(value_)) {
             return std::get<E>(std::move(value_));
         }
         return static_cast<E>(std::forward<G>(defaultError));
@@ -392,102 +392,211 @@ class Result<void, E> final
     using error_type = E;
 
     // Implementation - [SWS_CORE_00821]
-    Result() noexcept;
+    Result() noexcept = default;
 
     // Implementation - [SWS_CORE_00823]
-    explicit Result(const E& e);
+    explicit Result(const E& e)
+        : value_(e)
+    {}
 
     // Implementation - [SWS_CORE_00824]
-    explicit Result(E&& e);
+    explicit Result(E&& e)
+        : value_(std::move(e))
+    {}
 
     // Implementation - [SWS_CORE_00825]
-    Result(const Result& other);
+    Result(const Result& other) = default;
 
     // Implementation - [SWS_CORE_00826]
-    Result(Result&& other);
+    Result(Result&& other) = default;
 
     // Implementation - [SWS_CORE_00827]
     ~Result() noexcept = default;
 
     // Implementation - [SWS_CORE_00831]
-    static Result FromValue() noexcept;
+    static Result FromValue() noexcept { return Result(); }
 
     // Implementation - [SWS_CORE_00834]
-    static Result FromError(const E& e);
+    static Result FromError(const E& e) { return Result(e); }
 
     // Implementation - [SWS_CORE_00835]
-    static Result FromError(E&& e);
+    static Result FromError(E&& e) { return Result(std::move(e)); }
 
 
     // Implementation - [SWS_CORE_00836]
     template<typename... Args>
-    static Result FromError(Args&&... args);
+    static Result FromError(Args&&... args)
+    {
+        return Result(E(std::forward<Args>(args)...));
+    }
 
 
     // Implementation - [SWS_CORE_00841]
-    Result& operator=(const Result& other);
+    Result& operator=(const Result& other) = default;
 
 
     // Implementation - [SWS_CORE_00842]
     Result& operator=(Result&& other) noexcept(std::is_nothrow_move_constructible<E>::value &&
-                                               std::is_nothrow_move_assignable<E>::value);
+                                               std::is_nothrow_move_assignable<E>::value) = default;
 
     // Implementation - [SWS_CORE_00843]
     template<typename... Args>
-    void EmplaceValue(Args&&... args) noexcept;
+    void EmplaceValue(Args&&... args) noexcept{
+        value_.template emplace<void>(std::forward<Args>(args)...);
+    }
 
     // Implementation - [SWS_CORE_00844]
     template<typename... Args>
-    void EmplaceError(Args&&... args) noexcept;
+    void EmplaceError(Args&&... args) noexcept{
+        value_.template emplace<E>(std::forward<Args>(args)...);
+    }
     // Implementation - [SWS_CORE_00845]
     void Swap(Result& other) noexcept(std::is_nothrow_move_constructible<E>::value &&
-                                      std::is_nothrow_move_assignable<E>::value);
+                                      std::is_nothrow_move_assignable<E>::value){
+        value_.swap(other.value_);
+    }
 
     // Implementation - [SWS_CORE_00851]
-    bool HasValue() const noexcept;
+    bool HasValue() const noexcept{
+        return false;
+    }
 
     // Implementation - [SWS_CORE_00852]
-    explicit operator bool() const noexcept;
+    explicit operator bool() const noexcept{
+        return HasValue();
+    }
 
     // Implementation - [SWS_CORE_00853]
-    void operator* () const;
+    void operator*() const{
+        if (HasValue()) {
+            //[SWS_CORE_00021] - Violation is non-recoverable.
+            // TODO: LOG PRINT
+            std::terminate();
+        }
+    }
 
     // Implementation - [SWS_CORE_00855]
-    void Value () const;
+    void Value() const{
+        if (!HasValue()) {
+            //[SWS_CORE_00021] - Violation is non-recoverable.
+            // TODO: LOG PRINT
+            std::terminate();
+        }
+    }
 
     // Implementation - [SWS_CORE_00857]
-    const E & Error () const &;
+    const E& Error() const&{
+        if(std::holds_alternative<E>(value_)){
+            return std::get<E>(value_);
+        }
+        //[SWS_CORE_00021] - Violation is non-recoverable.
+        // TODO: LOG PRINT
+        std::terminate();
+    }
 
     // Implementation - [SWS_CORE_00876]
-    E & Error () &;
+    E& Error() &{
+        if(std::holds_alternative<E>(value_)){
+            return std::get<E>(value_);
+        }
+        //[SWS_CORE_00021] - Violation is non-recoverable.
+        // TODO: LOG PRINT
+        std::terminate();
+    }
 
     // Implementation - [SWS_CORE_00858]
-    E && Error () &&;
+    E&& Error() &&{
+        if(std::holds_alternative<E>(value_)){
+            return std::get<E>(std::move(value_));
+        }
+        //[SWS_CORE_00021] - Violation is non-recoverable.
+        // TODO: LOG PRINT
+        std::terminate();
+    }
 
     // Implementation - [SWS_CORE_00868]
-    std::optional<E> Err () const &;
+    std::optional<E> Err() const&{
+        if(std::holds_alternative<E>(value_)){
+            return std::get<E>(value_);
+        }
+        return std::nullopt;
+    }
 
     // Implementation - [SWS_CORE_00869]
-    std::optional<E> Err () &&;
+    std::optional<E> Err() &&{
+        if(std::holds_alternative<E>(value_)){
+            return std::get<E>(std::move(value_));
+        }
+        return std::nullopt;
+    }
 
 
     // Implementation - [SWS_CORE_00861]
-    template <typename U>
-    void ValueOr (U &&defaultValue) const{
+    template<typename U>
+    void ValueOr(U&& defaultValue) const
+    {
         // This function only exists for helping with generic programming
     }
 
 
     // Implementation - [SWS_CORE_00863]
-    template <typename G>
-    E ErrorOr (G &&defaultError) const &;
+    template<typename G>
+    E ErrorOr(G&& defaultError) const&{
+        if (std::holds_alternative<E>(value_)) {
+            return std::get<E>(value_);
+        }
+        return static_cast<E>(std::forward<G>(defaultError));
+
+    }
 
     // Implementation - [SWS_CORE_00864]
+    template<typename G>
+    E ErrorOr(G&& defaultError) &&{
+        if (std::holds_alternative<E>(value_)) {
+            return std::get<E>(std::move(value_));
+        }
+        return static_cast<E>(std::forward<G>(defaultError));
+    }
+
+
+    // Implementation - [SWS_CORE_00865]
     template <typename G>
-    E ErrorOr (G &&defaultError) &&;
+    bool CheckError (G &&error) const{
+        return Error() == static_cast<E>(std::forward<G>(error));
+    }
+
+    // Implementation - [SWS_CORE_00866]
+    void ValueOrThrow() const noexcept(false){
+        throw std::runtime_error("Result has no value");
+    }
+
+    // Implementation - [SWS_CORE_00867]
+    template<typename F>
+    void Resolve(F&& f) const{
+        return std::forward<F>(f)(Error());
+    }
 
 
+    // Implementation - [SWS_CORE_00870]
+    template<typename F>
+    auto Bind(F&& f) const{
+        using ReturnType = decltype(std::forward<F>(f)(Error()));
+        if (std::holds_alternative<E>(value_)) {
+            if constexpr (std::is_same_v<ReturnType, Result>) {
+                return f(Error());
+            }
+            else {
+                return Result<ReturnType, E>(f(Error()));
+            }
+        }
+        else {
+            return Result<ReturnType, E>(Error());
+        }
+    }
 
+
+private:
+    std::variant<void, E> value_;
 };
 
 
